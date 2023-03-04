8e88172205bf78838bf23f2a34796cabbec05622
Ritik Ranjan <rritik772@outlook.com>
2023-03-04 22:20:12 +0530

    in CustomerInfoRestController added 404 error and banner editer

--- server/customer/src/main/java/com/bankasarus/customer/controllers/CustomerInfoRestController.java
+++ server/customer/src/main/java/com/bankasarus/customer/controllers/CustomerInfoRestController.java
@@ -4,32 +4,36 @@ import com.bankasarus.customer.exceptions.UserNotFoundException;
 import com.bankasarus.customer.models.Customer;
 import com.bankasarus.customer.services.CustomerDataAccessService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerInfoRestController {
 
     @Autowired
-    CustomerDataAccessService customerService;
+    CustomerDataAccessService service;
 
     @Autowired
     RestTemplate restTemplate;
 
 
     @GetMapping("{email}")
-    public Customer getCustomerByEmail(@PathVariable("email") String email) {
-        return customerService.getCustomerByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException("User Not Found!!!"));
+    public ResponseEntity<Customer> getCustomerByEmail(@PathVariable("email") String email) {
+        Optional<Customer> customer = service.getCustomerByEmail(email);
+        return customer.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(404).body(null));
     }
 
     @GetMapping("{email}/has-account")
     public boolean isCustomerHavingAccount(@PathVariable("email") String email) {
-        return customerService.isCustomerHavingAccount(email);
+        return service.isCustomerHavingAccount(email);
     }
 
     @GetMapping("{email}/account")
--- server/customer/src/main/java/com/bankasarus/customer/repositories/CustomerRepository.java
+++ server/customer/src/main/java/com/bankasarus/customer/repositories/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.bankasarus.customer.repositories;
 
 import com.bankasarus.customer.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(String email);
 }
