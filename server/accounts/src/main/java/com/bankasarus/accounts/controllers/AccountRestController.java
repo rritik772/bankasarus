edcb4f91e9740046e208a58e4ff848e3b1bf7af6
Ritik Ranjan <rritik772@outlook.com>
2023-03-06 21:58:34 +0530

    seperated account and transaction in accounts module

--- server/accounts/src/main/java/com/bankasarus/accounts/controllers/AccountRestController.java
+++ server/accounts/src/main/java/com/bankasarus/accounts/controllers/AccountRestController.java
@@ -1,14 +1,11 @@
 package com.bankasarus.accounts.controllers;
 
 import com.bankasarus.accounts.models.Account;
-import com.bankasarus.accounts.models.Transaction;
 import com.bankasarus.accounts.services.AccountDataAccessService;
-import com.bankasarus.accounts.services.TransactionDataAccessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,43 +13,20 @@ import java.util.Optional;
 public class AccountRestController {
 
     @Autowired
-    AccountDataAccessService accountDataAccessService;
-
-    @Autowired
-    TransactionDataAccessService transactionDataAccessService;
+    AccountDataAccessService service;
 
     @GetMapping("{id}")
     public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
-        Optional<Account> account = accountDataAccessService.findAccountById(id);
+        Optional<Account> account = service.findAccountById(id);
         return account.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(404).body(null));
     }
 
     @GetMapping("{email}/by-email")
     public ResponseEntity<Account> getAccountByEmail(@PathVariable String email){
-        Optional<Account> account = accountDataAccessService.findAccountByEmail(email);
+        Optional<Account> account = service.findAccountByEmail(email);
         return account.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(404).body(null));
-    }
-
-    @GetMapping("{email}/transactions")
-    public ResponseEntity<List<Transaction>> getAllTransaction(@PathVariable String email) {
-        return ResponseEntity.ok(transactionDataAccessService.getTransactionByAccountId(email));
-    }
-
-    @GetMapping("{email}/mini-statement")
-    public ResponseEntity<List<Transaction>> getMiniStatement(@PathVariable String email) {
-        return ResponseEntity.ok(transactionDataAccessService.getLastSixTransactionsById(email));
-    }
-
-    @GetMapping("{email}/month-transactions")
-    public ResponseEntity<List<Transaction>> getThisMonthTransaction(@PathVariable String email) {
-        return ResponseEntity.ok(transactionDataAccessService.getTransactionsForThisMonth(email));
-    }
-
-    @PostMapping
-    public ResponseEntity<Transaction> insertTransaction(@RequestBody Transaction transaction) {
-        return ResponseEntity.ok(transactionDataAccessService.insertTransaction(transaction));
     }
 
 }
---
+++ server/accounts/src/main/java/com/bankasarus/accounts/controllers/TransactionRestController.java
@@ -0,0 +1,37 @@
+package com.bankasarus.accounts.controllers;
+
+import com.bankasarus.accounts.models.Transaction;
+import com.bankasarus.accounts.services.TransactionDataAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionRestController {
+    @Autowired
+    TransactionDataAccessService service;
+
+    @GetMapping("{email}/transactions")
+    public ResponseEntity<List<Transaction>> getAllTransaction(@PathVariable String email) {
+        return ResponseEntity.ok(service.getTransactionByAccountId(email));
+    }
+
+    @GetMapping("{email}/mini-statement")
+    public ResponseEntity<List<Transaction>> getMiniStatement(@PathVariable String email) {
+        return ResponseEntity.ok(service.getLastSixTransactionsById(email));
+    }
+
+    @GetMapping("{email}/month-transactions")
+    public ResponseEntity<List<Transaction>> getThisMonthTransaction(@PathVariable String email) {
+        return ResponseEntity.ok(service.getTransactionsForThisMonth(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> insertTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(service.insertTransaction(transaction));
+    }
+
+}
